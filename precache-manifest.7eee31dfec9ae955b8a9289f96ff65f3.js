8941def00c01d9e7f4c719657ed9d3880c428d52
liamDelaney <lgd34@cornell.edu>
2018-12-03 19:11:42 -0500

    test?

---
+++ precache-manifest.7eee31dfec9ae955b8a9289f96ff65f3.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "9dea42f346c4674bb346650751aac99d",
+    "url": "/img/2minos.9dea42f3.jpg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4edebf9ca801fd99ad5569dfcada0ea4",
+    "url": "/img/4river.4edebf9c.jpg"
+  },
+  {
+    "revision": "6399047ebb8ffd1003d7",
+    "url": "/css/chunk-vendors.23d4896f.css"
+  },
+  {
+    "revision": "01d328d22a22215e9be4918b35dc3c2e",
+    "url": "/img/1limbo.01d328d2.jpg"
+  },
+  {
+    "revision": "161b3b0894da11aba33289f1749aaf5c",
+    "url": "/img/5anger.161b3b08.jpg"
+  },
+  {
+    "revision": "34c57faa8cfae1c117d4aa3ebf74cc33",
+    "url": "/img/2lust.34c57faa.jpg"
+  },
+  {
+    "revision": "30ef9b2f0ad1da0dee6e138db5360efe",
+    "url": "/img/3gluttony.30ef9b2f.jpg"
+  },
+  {
+    "revision": "3db66a7c8a765a23a88d37e8ee758d5a",
+    "url": "/img/5dis.3db66a7c.jpg"
+  },
+  {
+    "revision": "cc17104fd60a0693c7a02c7dc6377ca4",
+    "url": "/img/4fourthCourt.cc17104f.jpg"
+  },
+  {
+    "revision": "8dfb3bc3bb72bfdbdee8",
+    "url": "/js/app.51f2709d.js"
+  },
+  {
+    "revision": "6399047ebb8ffd1003d7",
+    "url": "/js/chunk-vendors.7a83ef85.js"
+  },
+  {
+    "revision": "229659269afbe30180e2639055ac197d",
+    "url": "/img/5fifthCourt.22965926.jpg"
+  },
+  {
+    "revision": "453203b58a3f3cb6ece4f644fb542f3c",
+    "url": "/img/intro.453203b5.jpg"
+  },
+  {
+    "revision": "ae073432b4840f3f2781c258d867d643",
+    "url": "/img/2secondCourt.ae073432.jpg"
+  },
+  {
+    "revision": "ce5b039497359ead79c53a685bdb7722",
+    "url": "/img/3thirdCourt.ce5b0394.jpg"
+  },
+  {
+    "revision": "f05732dd1c338b82e0eaa140925a6e67",
+    "url": "/img/1firstCourt.f05732dd.jpg"
+  },
+  {
+    "revision": "4e25d95d43788cbd8de7f38ad844c645",
+    "url": "/img/4greed.4e25d95d.jpg"
+  },
+  {
+    "revision": "3b4a36332aa082babff4a55a80cd4835",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dfb3bc3bb72bfdbdee8",
+    "url": "/css/app.9633bf4f.css"
+  }
+];
